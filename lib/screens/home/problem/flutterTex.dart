4d82370a8cdc6f7c47af3708ad10189461eb68d1
Abdullahfoysal <abdullah32@student.sust.edu>
2021-04-28 11:35:58 +0600

    rendering engine selector added

--- lib/screens/home/problem/flutterTex.dart
+++ lib/screens/home/problem/flutterTex.dart
@@ -3,67 +3,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_tex/flutter_tex.dart';
 import 'package:srmcapp/models/problemAndSolution.dart';
 
-/*class FlutterTex extends StatefulWidget {
-  @override
-  _FlutterTexState createState() => _FlutterTexState();
-}
-
-class _FlutterTexState extends State<FlutterTex> {
-  @override
-  Widget build(BuildContext context) {
-    return TeXView(
-        renderingEngine: TeXViewRenderingEngine.katex(),
-        children: [
-          TeXViewContainer(id: "container_0", children: [
-            TeXViewTeX(
-              r"<h3>Title fg ada jadga gdadka fg ada jadga gdadka adga gdadka adga gdadka  fg ada jadga gdadka dgadah  jadga gdadka dgadah  jadga gdadka dgadah d</h3>",
-              style: TeXViewStyle(
-                textAlign: TeXViewTextAlign.Center,
-                backgroundColor: Colors.white,
-                contentColor: Colors.black,
-              ),
-              id: "tex_0",
-            ),
-            TeXViewTeX(r"""<p>                                
-                           When \(a \ne 0 \), there are two solutions to \(ax^2 + bx + c = 0\) and they are
-                           $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$</p>""",
-                style: TeXViewStyle.fromCSS(
-                    "color:black;background-color:white;padding:5px;"),
-                id: "tex_1"),
-          ])
-        ],
-        style: TeXViewStyle(
-          margin: TeXViewMargin.all(5),
-          elevation: 5,
-          borderRadius: TeXViewBorderRadius.all(10),
-          border: TeXViewBorder.all(
-            TeXViewBorderDecoration(
-                borderColor: Colors.white,
-                borderStyle: TeXViewBorderStyle.Solid,
-                borderWidth: 5),
-          ),
-          backgroundColor: Colors.white,
-        ),
-        loadingWidget: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircularProgressIndicator(),
-              Text("Rendering with ")
-            ],
-          ),
-        ),
-        onTap: (childID) {
-          print("TeXView $childID is tapped.");
-        });
-  }
-}*/
 class FlutterTex extends StatelessWidget {
   final ProblemAndSolution problemAndSolution;
+  final TeXViewRenderingEngine renderingEngine;
 
-  FlutterTex(this.problemAndSolution);
+  FlutterTex(this.renderingEngine, this.problemAndSolution);
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +16,7 @@ class FlutterTex extends StatelessWidget {
     temp = problemAndSolution.problemText;
     String problemText = '<p>$temp</p>';
     return TeXView(
-        renderingEngine: TeXViewRenderingEngine.mathjax(),
+        renderingEngine: renderingEngine,
         children: [
           TeXViewContainer(id: "container_0", children: [
             TeXViewTeX(
--- lib/screens/home/problem/problemProfile.dart
+++ lib/screens/home/problem/problemProfile.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_tex/flutter_tex.dart';
+import 'package:group_radio_button/group_radio_button.dart';
 import 'package:srmcapp/designs/submitSolution.dart';
 import 'package:srmcapp/models/problemAndSolution.dart';
 import 'package:srmcapp/screens/home/problem/flutterTex.dart';
 import 'package:srmcapp/screens/home/problem/problemHeader.dart';
 import 'package:srmcapp/services/user/userActivity.dart';
 import 'package:srmcapp/shared/colors.dart';
+import 'package:srmcapp/shared/constant.dart';
 
-class ProblemProfile extends StatelessWidget {
+class ProblemProfile extends StatefulWidget {
   final ProblemAndSolution problemAndSolution;
   final UserActivity userActivity;
   final int problemNumber;
@@ -18,6 +21,20 @@ class ProblemProfile extends StatelessWidget {
     this.problemNumber,
     this.userActivity,
   });
+
+  @override
+  _ProblemProfileState createState() => _ProblemProfileState();
+}
+
+class _ProblemProfileState extends State<ProblemProfile> {
+  TeXViewRenderingEngine renderingEngine = TeXViewRenderingEngine.mathjax();
+
+  TeXViewRenderingEngine _verticalGroupValue = TeXViewRenderingEngine.mathjax();
+
+  List<TeXViewRenderingEngine> _status = [
+    TeXViewRenderingEngine.mathjax(),
+    TeXViewRenderingEngine.katex()
+  ];
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,13 +53,34 @@ class ProblemProfile extends StatelessWidget {
               //TODO-2: problem header[catagory,rating,setter]
               Container(
                 height: 90.0,
-                child: ProblemHeader(problemNumber, problemAndSolution),
+                child: ProblemHeader(
+                    widget.problemNumber, widget.problemAndSolution),
+              ),
+              Text(
+                'Rendering with',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              RadioGroup<TeXViewRenderingEngine>.builder(
+                // value: _verticalGroupValue,
+                direction: Axis.horizontal,
+                groupValue: _verticalGroupValue,
+                onChanged: (value) => setState(() {
+                  _verticalGroupValue = value;
+                  renderingEngine = value;
+                  //print();
+                }),
+                items: _status,
+                itemBuilder: (item) => RadioButtonBuilder(item.getEngineName()),
               ),
               Container(
                 padding: EdgeInsets.all(8.0),
-                child: FlutterTex(problemAndSolution),
+                child: FlutterTex(renderingEngine, widget.problemAndSolution),
               ),
-              SubmitSolution(problemAndSolution, userActivity, problemNumber),
+              SubmitSolution(widget.problemAndSolution, widget.userActivity,
+                  widget.problemNumber),
 
               //title & problems
 
